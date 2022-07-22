d816baa12a6c362931261ac538a19f59eaf7921a
bhb-boy <bernhard_beetz@gmx.de>
2022-07-22 19:10:27 +0200

    functions_mit_pdf

--- 36_abschluss_funktionen.py
+++ 36_abschluss_funktionen.py
@@ -2,82 +2,27 @@
 # Vervollständige die Funktion list_sum(), der als Parameter eine Liste mit den Preisen übergeben wird.
 # Die Funktion soll dann die Summe der Zahlen aus der Liste ausgeben.
 #
-#cart_prices = [20, 3.5, 6.49, 8.99, 9.99, 14.98]
-#sum_of_prices = 0
-
-# BHB Lösung mit "for" loop, ohne function
-        #for price in cart_prices:
-        #    sum_of_prices = sum_of_prices + price
-        #print(sum_of_prices)
 
 """cart_prices = [20, 3.5, 6.49, 8.99, 9.99, 14.98]
 
-def list_sum(l):    #   Aufruf Funktionsname(Parameter):
-    total = 0       # counter auf "0" setzen
-    for i in l:     # Schleife "i" innerhalb der funktion
+def list_sum(l):            #   Aufruf Funktionsname(Parameter):
+    total = 0               # counter auf "0" setzen
+    for i in l:             # Schleife "i" innerhalb der funktion. Hier wird der function name (l) aufgerufen
         total = total + i   # Addition er enthaltenen Artikel
     print(total)
     # alternativ einfach: print(sum(l)) # Hier mit "sum" Funktion
 
-list_sum(cart_prices)   # Aufruf der Funktion! call the function
+list_sum(cart_prices)   # Aufruf der Funktion! Hier wird die Varaible ausserhalb der Funktion übergeben (cart_prices)
 # Folgende Ausgabe wird erwartet: 63.95"""
 
-# b.) Schreibe eine Funktion, die für einen Artikel eine Preis-Tabelle erstellt!
-# Liste mit Preisen bis zehn Einheiten des Artikels enthalten
-# So soll jedes Element in der Liste so aussehen: "Anzahl x Artikel: Preis".
-
-"""articel = "Wunderkeks"
-price = "0,79"
-max_articel = 10
-
-#def prices_list(l):
-count = 0
-while count <= 10:
-    print(count)
-    count = count + 1
-
-prices_list(count)"""
-# Ein zarter Anfang der funktioniert
-def my_function():
-    print("Hello from my function")
-my_function()
-
-article = "Wunderkeks"
-price = "0,79"
-max_article = 10
-
-"""def prices_list():          # function ohne argument()
-    count = 0
-    if count <= 10:
-        print(articel, price)
-        count = count + 1
-        print("Hier geht mehr rein")
-        print(count)
-    else:
-        print("Wir haben mehr als einen Artikel")"""
-
-
-def tolle_funktion():
-    price = 0.79
-    count = 2
-"""    while count <= 10:
-        print(article,price)
-        count = count + 1
-        new_price = (float(price)) + (float(price))   # Hier soll der Preis addiert werden
-#        print(new_price)
-        next_price = (float(new_price)) + (float(price))    # Hier soll der "new_price" wieder um "price" addiert werden
-        print(new_price )
-        print(next_price)
-        print(count)
-tolle_funktion()"""
-
-#prices_list()       # call der function
+# b.) Funktion, der sie einen Artikelnamen und den Verkaufspreis übergeben kann.
+# Daraus soll die Funktion eine Liste erstellen, in der die Preise von einem, zwei, drei,... bis zehn Einheiten
+# des Artikels stehen. Genauer soll jedes Element in der Liste so aussehen: "Anzahl x Artikel: Preis".
 
 # Hier die Doofmannlösung
-price = 0.79
+"""price = 0.79
 
 def counting():
-#if count <= 10:
     count = 0
     count = count + 1
     print(price, "1 Artikel")
@@ -100,12 +45,94 @@ def counting():
     print(next_price,"9 Artikel")
     next_price = (float(next_price)) + (float(price))
     print(next_price,"10 Artikel")
-#else:
-#    print("Done")
-counting()
 
-# Ich brauche 2 Funktionen, 1 für die erste "price + price" und eine weitere "next_price"
+counting()"""
+
+# Hier die lbr Lösung
+"""article = "Wunderkeks"  # Artikel wird ausserhalb der Funktion übergeben
+price = 0.79              # Preis wird auch ausserhalb übergeben
+
+def prices_list():
+    liste = []
+    count = 0
+    while count <= 9:
+        count = count + 1                               # Counter funzt
+        price_sum = count * float(price)                # Aufsteigender Preis anhand von count * price
+        liste.append(f"{count},{article},{price_sum}")  # Jetzt als Listenausgabe mit f (format) und String Formattierung
+    return liste
+
+liste = prices_list()                                   # Hier wird die function an eine Variable übergeben
+                                                        # um an eine Ausgabe zu kommen
+print(liste)
+
+# Ausgabe in eine Liste, append hängt doppelte hintereinader, join schmeisst doppelte raus"""
+
+# b.) Funktion, der sie einen Artikelnamen und den Verkaufspreis übergeben kann.
+# Daraus soll die Funktion eine Liste erstellen, in der die Preise von einem, zwei, drei,... bis zehn Einheiten
+# des Artikels stehen. Genauer soll jedes Element in der Liste so aussehen: "Anzahl x Artikel: Preis".
+
+# Hier das final
+"""name = "Wunderkeks"
+price = 0.79
+
+def prices_list(name, price):   # Parameter Übergabe aus einer Variable in ()
+    count = 0
+    if count <= 10:
+        count = count + 1
+    return count
+    count = zaehler
+    print(zaehler)"""
 
 
+# Tests auf der zweiten Runde des Tutorial. Das Lesen der Arbeitsblätter ist wichtig, die Videos sind zu wenig.
+#def funktionsname(Argument):
 
+def nardo_funct(wirduntenübergeben):    # Funktionsname nardo_funct mit Argument "wirdübegeben"
+    print(wirduntenübergeben)
+nardo_funct("Deine Mudder")     # Hier findet die Parameter/Argument Übergabe der Variable statt
+                                # die in der Function verwendet werden soll
+# Ausgabe:
+#Deine Mudder
 
+# 2 Aufrufe der gleichen Function "nardo_funct" mit verschiedenen Parameter
+nardo_funct("Eine Varaible")
+nardo_funct("Eine andere Variable")
+# Ausgabe
+#Eine Varaible
+#Eine andere Variable
+
+# Funktion mit zwei Argumenten
+def argument_funct(name, count):
+    for i in range(0, count):       # counter wird auf "0" gesetzt
+        print(name)                 # Was der hier soll weiß ich nicht
+argument_funct("Mehr!", 4)          # Funktionsaufruf mit Argument "Mehr!" und count = 4
+# Ausgabe:
+#Mehr!
+#Mehr!
+#Mehr!
+#Mehr!
+
+# Veschachtelung von Funktionen
+def schachtel():
+    nardo_funct("Nardo seine erste Funktion")
+    argument_funct("wiederholt sich immer wieder", 3)
+schachtel()
+# Ausgabe:
+#Nardo seine erste Funktion
+#wiederholt sich immer wieder
+#wiederholt sich immer wieder
+#wiederholt sich immer wieder
+
+# len Function
+var1 = len("Vier Bier")  # Zählt die Anzahl der Elemente eines einzelnen String
+var2 = len(["Vier Bier", "und", "eine", "Suppe"])  # Zählt die Anzahl der Listenelemente
+print(var1)
+print(var2)
+
+# return statement = Functions send Python values/Objects back to the caller.
+#                    These values/objects are known as the function's return value"""
+"""
+def multiply(number1, number2):
+    return number1 * number2
+x = multiply(6,8)       # set variable to print the return code
+print(x)"""
